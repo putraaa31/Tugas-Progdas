13262ede75e6386fe2724cb53cec9ad4d04fe7d4
putraaa31 <88871794+putraaa31@users.noreply.github.com>
2021-09-01 18:33:44 +0700

    Praktek Perpustakaan

--- Tugas praktek/Perpustakaan Online/Perpustakaan.js
+++ Tugas praktek/Perpustakaan Online/Perpustakaan.js
@@ -1,6 +1,6 @@
-var namabuku = ["Harry Potter","Dilan","Bumi","Selena","Nebula","Laskar Pelangi","Si Putih","Bulan"]
-      var bataswaktupinjam = ["7 Hari","7 Hari","7 Hari","7 Hari","7 Hari","7 Hari","7 Hari","7 Hari"]
-      var gambarbuku = ["https://upload.wikimedia.org/wikipedia/id/5/56/Harry_potter_deathly_hallows_US.jpg","https://www.gramedia.com/blog/content/images/2019/02/dilan-1990.jpeg","https://upload.wikimedia.org/wikipedia/id/thumb/4/49/Bumi_%28sampul%29.jpg/220px-Bumi_%28sampul%29.jpg","https://tasyrifunnisa.files.wordpress.com/2020/06/inshot_20200630_165330032.jpg?w=686","https://i.pinimg.com/originals/eb/dc/29/ebdc2929db1d3bbfc9cf220e2d7acd7b.jpg","https://upload.wikimedia.org/wikipedia/id/thumb/8/8e/Laskar_pelangi_sampul.jpg/220px-Laskar_pelangi_sampul.jpg","https://digibunda.com/wp-content/uploads/2020/11/Novel-PDF-Free-Novel-Si-Putih-Unedited-Version-by-Tere-Liye.jpg","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBXAYBO7AymsDJMDd9JrteHnJPuICRkjyPThaQoCcFU2rjU_AAlXi8b2mYb-3ZaLEPXKM&usqp=CAU"]
+var namabuku = ["Matahari","Hujan","Bumi","Selena","Nebula","Pergi","Bintang","Bulan","Komet","Selamat Tinggal"]
+      var bataswaktupinjam = ["7 Hari","7 Hari","7 Hari","7 Hari","7 Hari","7 Hari","7 Hari","7 Hari","7 Hari","7 Hari"]
+      var gambarbuku = ["https://2.bp.blogspot.com/-7hsQNKWv2hg/V9eYPWS6olI/AAAAAAAABQY/MfpWoTMxcccM_U3SaGGdGqwaLAOeMukJQCLcB/s1600/Sampul%2BMatahari.jpg","https://4.bp.blogspot.com/-jlUCGh6UZtU/XEo5BOtkUzI/AAAAAAAAAE4/H7hLa-XyN1Q2y6C3LgKWhKyw-OrHtS5gQCLcBGAs/s1600/hujan%2Btere%2Bliye.jpg","https://upload.wikimedia.org/wikipedia/id/thumb/4/49/Bumi_%28sampul%29.jpg/220px-Bumi_%28sampul%29.jpg","https://tasyrifunnisa.files.wordpress.com/2020/06/inshot_20200630_165330032.jpg?w=686","https://i.pinimg.com/originals/eb/dc/29/ebdc2929db1d3bbfc9cf220e2d7acd7b.jpg","https://assets-a1.kompasiana.com/items/album/2018/08/18/14ce416308240e90767f9eed91930d1f2bc21207-1-5b784869bde5753b1a0246f2.jpg","https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1496315625l/35278713._SX318_.jpg","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBXAYBO7AymsDJMDd9JrteHnJPuICRkjyPThaQoCcFU2rjU_AAlXi8b2mYb-3ZaLEPXKM&usqp=CAU","https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1531141646l/40093223._SX318_.jpg","https://inc.mizanstore.com/aassets/img/com_cart/produk/grmk-017--.jpg"]
       var banyakbuku = [1,2,3,4,5,6,7,8,]
 
       var listproduk = document.getElementById("listproduk")
@@ -17,12 +17,12 @@ var namabuku = ["Harry Potter","Dilan","Bumi","Selena","Nebula","Laskar Pelangi"
         listproduk.innerHTML =""
 
         for ( let i = 0; i < namabuku.length; i++){
-          listproduk.innerHTML += '<div class="card float-left mr-3 mb-3" style="width: 12rem;">'+
+          listproduk.innerHTML += '<div class="card float-left mr-2  mb-3 text-white bg-dark" style="width: 11rem;">'+
             '<img src="'+gambarbuku[i]+'" class="card-img-top" alt="...">'+
             '<div class="card-body">'+
               '<h5 class="card-title">'+namabuku[i]+'</h5>'+
               '<p class="card-text">'+bataswaktupinjam[i]+'</p>'+
-              '<a href="#" class="btn btn-primary" onclick="addlistitem('+i+')">Pinjam</a>'+
+              '<a href="#" class="btn btn-light" onclick="addlistitem('+i+')">Pinjam</a>'+
             '</div>'
         }
 
@@ -74,6 +74,7 @@ var namabuku = ["Harry Potter","Dilan","Bumi","Selena","Nebula","Laskar Pelangi"
         var nohp = []
         var email = []
         var buku = []
+        var date = []
         var waktu = []
 
         var tampil = document.getElementById("tampildata")
@@ -90,6 +91,7 @@ var namabuku = ["Harry Potter","Dilan","Bumi","Selena","Nebula","Laskar Pelangi"
                 "<td>"+nohp[i]+"</td>"+
                 "<td>"+email[i]+"</td>"+
                 "<td>"+buku[i]+"</td>"+
+                "<td>"+date[i]+"</td>"+
                 "<td>"+waktu[i]+"</td>"+
                 "<td>"+
                     "<button class='btn btn-warning'onclick='edit("+i+")'>Edit</button>"+
@@ -105,12 +107,15 @@ var namabuku = ["Harry Potter","Dilan","Bumi","Selena","Nebula","Laskar Pelangi"
             var namabaru = document.getElementById("txt_nama").value
             var nohpbaru = document.getElementById("txt_nohp").value
             var emailbaru = document.getElementById("txt_email").value
+            var datebaru = document.getElementById("txt_date").value
             var waktubaru = document.getElementById("txt_waktu").value
             var bukubaru = document.getElementById("listkeranjang")
+
 
             nama.push(namabaru)
             nohp.push(nohpbaru)
             email.push(emailbaru)
+            date.push(datebaru)
             waktu.push(waktubaru+" Hari")
             buku.push(namabuku_keranjang)
 
@@ -126,6 +131,7 @@ var namabuku = ["Harry Potter","Dilan","Bumi","Selena","Nebula","Laskar Pelangi"
             nama.splice(i,1)
             nohp.splice(i,1)
             email.splice(i,1)
+            date.splice(i,1)
             waktu.splice(i,1)
 
             showData()
@@ -135,11 +141,13 @@ var namabuku = ["Harry Potter","Dilan","Bumi","Selena","Nebula","Laskar Pelangi"
             var namabaru = document.getElementById("txt_nama").value
             var nohpbaru = document.getElementById("txt_nohp").value
             var emailbaru = document.getElementById("txt_email").value
+            var datebaru = document.getElementById("txt_date").value
             var waktubaru = document.getElementById("txt_waktu").value
 
             nama.splice(i,1,namabaru)
             nohp.splice(i,1,nohpbaru)
             email.splice(i,1,emailbaru)
+            date.splice(i,1,datebaru)
             waktu.splice(i,1,waktubaru+" Hari")
 
             showData()
